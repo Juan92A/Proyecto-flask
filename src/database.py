85b6add35a2a300aedb72ad576f34f31928786ef
Juan92A <acunajuan429@gmail.com>
2023-05-13 13:18:10 -0600

    validacion db

--- src/database.py
+++ src/database.py
@@ -1,8 +1,12 @@
 import mysql.connector
 
-database = mysql.connector.connect(
-    host='localhost',
-    user ='root',
-    password = '',
-    database='usuarios'
-)
+try:
+    database = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='',
+        database='usuarios'
+    )
+    print("Conexión exitosa a la base de datos")
+except mysql.connector.Error as error:
+    print("Error al conectarse a la base de datos: {}".format(error))
