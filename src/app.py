e6e79afd99892176b09a4df5b1c24dd4056e3891
Juan92A <acunajuan429@gmail.com>
2023-05-13 11:58:40 -0600

    exceptions

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,redirect,url_for, request
+from flask import Flask, render_template, redirect, url_for, request
 import os
 import database as db
 
@@ -6,72 +6,76 @@ import database as db
 template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 
 #unir carpetas a directorio app
-template_dir = os.path.join(template_dir,'src','templates')
+template_dir = os.path.join(template_dir, 'src', 'templates')
 #instancia de directorio para lanzamiento en servidor
-app = Flask(__name__,template_folder=template_dir)
+app = Flask(__name__, template_folder=template_dir)
 
-def hide_chars(value):
-    """
-    Oculta todos los caracteres en el valor proporcionado.
-    """
-    return '*' * len(value)
-app.jinja_env.filters['hide_chars'] = hide_chars
+
 
 #ruta app
 @app.route('/')
 def home():
-    cursor  = db.database.cursor()
-    cursor.execute("select * from usuarios")
-    resultado = cursor.fetchall()
-    #datos a diccionarios
-    insertObject = []
-    columName = [column[0] for column in cursor.description]
-    for dato in resultado:
-        insertObject.append(dict(zip(columName,dato)))
-
-    cursor.close()
-    return render_template('index.html',data=insertObject)
-
-#guardarusuario
-@app.route('/user',methods=['POST'])
-def addUser():
-    usuario = request.form['usuario']
-    nombre = request.form['nombre']
-    contrasenia = request.form['pass']
-
-    if usuario and nombre and contrasenia:
+    try:
         cursor = db.database.cursor()
-        sql = "INSERT INTO usuarios (usuario, nombre, contrasenia) values (%s,%s,%s)"
-        data= (usuario,nombre,contrasenia)
-        cursor.execute(sql,data)
-        db.database.commit()
-    return redirect(url_for('home'))
+        cursor.execute("select * from usuarios")
+        resultado = cursor.fetchall()
+        #datos a diccionarios
+        insertObject = []
+        columName = [column[0] for column in cursor.description]
+        for dato in resultado:
+            insertObject.append(dict(zip(columName, dato)))
+        cursor.close()
+        return render_template('index.html', data=insertObject)
+    except Exception as e:
+        return str(e)
+
+#guardar usuario
+@app.route('/user', methods=['POST'])
+def addUser():
+    try:
+        usuario = request.form['usuario']
+        nombre = request.form['nombre']
+        contrasenia = request.form['pass']
+
+        if usuario and nombre and contrasenia:
+            cursor = db.database.cursor()
+            sql = "INSERT INTO usuarios (usuario, nombre, contrasenia) values (%s,%s,%s)"
+            data= (usuario, nombre, contrasenia)
+            cursor.execute(sql, data)
+            db.database.commit()
+        return redirect(url_for('home'))
+    except Exception as e:
+        return str(e)
 
 @app.route('/delete/<string:id>')
 def delete(id):
-    cursor = db.database.cursor()
-    sql = "DELETE FROM usuarios WHERE id=%s"
-    data = (id,)
-    cursor.execute(sql,data)
-    db.database.commit()
+    try:
+        cursor = db.database.cursor()
+        sql = "DELETE FROM usuarios WHERE id=%s"
+        data = (id,)
+        cursor.execute(sql, data)
+        db.database.commit()
+        return redirect(url_for('home'))
+    except Exception as e:
+        return str(e)
    
-    return redirect(url_for('home'))
-
 @app.route('/edit/<string:id>' ,methods=['POST'])
 def edit(id):
-    usuario = request.form['usuario']
-    nombre = request.form['nombre']
-    contrasenia = request.form['pass']
+    try:
+        usuario = request.form['usuario']
+        nombre = request.form['nombre']
+        contrasenia = request.form['pass']
 
-    if usuario and nombre and contrasenia:
-        cursor = db.database.cursor()
-        sql = "UPDATE usuarios SET usuario = %s, nombre=%s, contrasenia=%s WHERE id=%s"
-        data= (usuario,nombre,contrasenia,id)
-        cursor.execute(sql,data)
-        db.database.commit()
-    return redirect(url_for('home'))
-
+        if usuario and nombre and contrasenia:
+            cursor = db.database.cursor()
+            sql = "UPDATE usuarios SET usuario = %s, nombre=%s, contrasenia=%s WHERE id=%s"
+            data= (usuario, nombre, contrasenia, id)
+            cursor.execute(sql, data)
+            db.database.commit()
+        return redirect(url_for('home'))
+    except Exception as e:
+        return str(e)
 
 if __name__ == '__main__':
-    app.run(debug=True, port = 4000)
-
+    app.run(debug=True,port=40000)
+    
